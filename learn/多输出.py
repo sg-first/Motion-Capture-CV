82431e594e6c9e65ee7b65c7d02fa0736a8c8229
GPLme <524019351@qq.com>
2018-12-21 01:02:01 +0800

    改错 用`keras.models.Model`捏在一起的时候，`inputs`那个参数写的是**输入层**，不能写输入层连接了别的之后的模型。另外每个输出层输出维度是2不是8……之前说好多次了也没改。我都改完了

--- learn/多输出.py
+++ learn/多输出.py
@@ -1,25 +1,30 @@
 import keras
 import data
 import numpy as np
-
 from keras import backend as K
-model=keras.Sequential()
+
 input=keras.layers.Input((4,4))
-#model.add(keras.engine.input_layer.Input(shape=(4,4)))
+
+model=keras.Sequential()
 model.add(keras.layers.Flatten())
 model.add(keras.layers.Dense(12, input_shape=(16,),activation='relu')) # 添加全连接层
 model.add(keras.layers.Dense(10, input_shape=(12,),activation='relu'))
 model.add(keras.layers.Dense(9, input_shape=(10,),activation='softmax'))
-a=keras.layers.Dense(8, input_shape=(9,),activation='softmax')
-b=keras.layers.Dense(8, input_shape=(9,),activation='softmax')
-c=keras.layers.Dense(8, input_shape=(9,),activation='softmax')
-d=keras.layers.Dense(8, input_shape=(9,),activation='softmax')
+
 model1=model(input)
+
+a=keras.layers.Dense(2, input_shape=(9,),activation='softmax')
+b=keras.layers.Dense(2, input_shape=(9,),activation='softmax')
+c=keras.layers.Dense(2, input_shape=(9,),activation='softmax')
+d=keras.layers.Dense(2, input_shape=(9,),activation='softmax')
+
 model2=a(model1)
 model3=b(model1)
 model4=c(model1)
 model5=d(model1)
-e=keras.models.Model(inputs=model1,outputs=[model2,model3,model4,model5])
+
+e=keras.models.Model(inputs=input,outputs=[model2,model3,model4,model5])
+
 e.compile(optimizer='rmsprop',loss='hinge')
 e.fit(data.x, [data.y_armL,data.y_armR,data.y_legL,data.y_legR],epochs=1000, batch_size=2)
 print(e.evaluate(data.x,[data.y_armL,data.y_armR,data.y_legL,data.y_legR], batch_size=2))
