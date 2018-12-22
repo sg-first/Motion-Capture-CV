9f8e82f03ef1fac52cd567c4dc616f7d14c29975
GPLme <524019351@qq.com>
2018-12-23 00:39:08 +0800

    根据耿诗杰的测试改

--- learn/grid/grid.py
+++ learn/grid/grid.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # 参数为：待搜索模型、搜索范围、训练数据、验证数据、预测数据（x），验证损失阈值（大于阈值不进行predict验证）、（训练和验证的）batch大小
-def init_search(model,Range,TrainData,testData,x_pre,tho=99999,batch_size=-1):
+def init_search(model,Range,TrainData,testData,x_pre,tho=99999,batch_size=-1,isMulOut=False):
     if batch_size==-1:
         batch_size=len(TrainData[0])
         
@@ -10,7 +10,10 @@ def init_search(model,Range,TrainData,testData,x_pre,tho=99999,batch_size=-1):
         model.fit(TrainData[0], TrainData[1], epochs=1000, batch_size=batch_size)
         evalloss=model.evaluate(testData[0], testData[1], batch_size=batch_size)
         print(evalloss)
-        if(np.mean(evalloss)>tho):
+        cmploss=evalloss
+        if isMulOut:
+            cmploss=np.mean(evalloss)
+        if(cmploss>tho):
             continue
         else: # tho需要合理设置，然后到这基本就是行了
             y_test=model.predict(x_pre, batch_size=len(x_pre))
